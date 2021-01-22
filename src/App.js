196891f9d67e7ab5ed35a34bc45d69c5e09c2aa1
한영주 <h@han-yeongjuui-MacBookPro.local>
2021-01-23 02:07:02 +0900

    movieApp_test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./Router/Home";
+import About from "./Router/About";
+import Detail from "./Router/Detail";
+import Navigation from "./Router/Navigation";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail:id" component={Detail} />
+    </HashRouter>
   );
-}
+};
 
 export default App;
---
+++ src/Router/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <h1>어바웃페이지</h1>;
+};
+
+export default About;
